6ea6371ca8600dc8a7a5a3525ff65d42ece21253
Jair <jair.carmona87@gmail.com>
2022-07-28 13:31:00 -0500

    monkey

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div id="main-container">
+        <h1>Coolest Mint Ever</h1>
+
+        <div id="mint-button-container"></div>
+        <div id="mint-counter">counter</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
